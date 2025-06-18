cadf24115d4de9b14bd183697def1ed2dcf5ee23
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-18 14:47:47 +0530

    update

---
+++ Opp Concepts/Encapsulation&Abtraction/Ep1.cpp
@@ -0,0 +1,56 @@
+/*LEVEL 4:
+Create Class bankAccount-private Balance,deposite/withdraw method*/
+#include<iostream>
+using namespace std;
+class BankAccount
+{
+    private:
+    float balance;
+    public:
+    void showCurent() 
+    {
+        cout << "Total Balance:" << balance << endl;
+    }
+    BankAccount()
+    {
+        balance = 2500;
+    }
+    void Deposite(float amount)
+    {
+        if (amount > 0)
+        {
+            balance = balance + amount;
+            cout <<"Deposite :"  <<amount <<endl;
+        }
+        else
+        {
+            cout <<"Invalid Deposite Amount" <<endl;
+        }
+    }
+    void withdraw(float amount)
+    {
+        if (amount> 0 && amount <= balance)
+        {
+            balance = balance - amount;
+            cout <<"Withdraw :" <<amount <<endl;
+        }
+        else
+        {
+            cout <<"Insiffient Balance:" <<endl;
+        }
+    }
+     // Function to display current balance
+    void showBalance() 
+    {
+        cout << "Current Balance:" << balance << endl;
+    }
+};
+int main()
+{
+    BankAccount b1;
+    b1.showCurent();
+    b1.Deposite(500.90);
+    b1.withdraw(200.89);
+    b1.showBalance();
+    return 0;
+}
